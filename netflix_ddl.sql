eb42f4ba917063d7e876a4d1509eebb85cb1a1fe
Natalia <natallia.bahlai@gmail.com>
2024-07-19 21:53:50 +1000

    Add files via upload

---
+++ netflix_ddl.sql
@@ -0,0 +1,14 @@
+CREATE TABLE public.netflix_shows (
+    show_id text NOT NULL,
+    type text,
+    title text,
+    director text,
+    cast_members text,
+    country text,
+    date_added date,
+    release_year integer,
+    rating text,
+    duration text,
+    listed_in text,
+    description text
+);
